2b2f492b2263bfd382aa629052f69abc0f7963d7
Geher-Marcell <darkiex03@gmail.com>
2025-10-22 22:33:57 +0200

    Refactor Navbar component for improved layout and animation effects

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -9,28 +9,38 @@ export default function Navbar() {
 	return (
 		<>
 			<div
-				className={`w-screen h-fit p-4 ${
-					opened ? "pb-2" : "pb-0"
-				} bg-neutral-800`}
+				className={`fixed top-0 left-0 w-screen h-fit p-4 bg-neutral-800 z-50`}
 			>
 				<div className="flex justify-between items-center">
 					<h1 className="text-2xl font-bold">Valami cég</h1>
 
 					<motion.button
 						onClick={() => setIsOpen(!opened)}
-						className="space-y-2 *:w-6 *:h-0.5 *:bg-white p-2"
-						animate={{ rotate: opened ? 90 : 0 }}
-						transition={{ ease: "easeInOut", duration: 0.3 }}
+						className="space-y-2 *:w-7 *:h-0.5 *:bg-white p-2"
 					>
-						<div></div>
-						<div></div>
-						<div></div>
+						<motion.div
+							className="origin-right"
+							animate={{ rotate: opened ? -45 : 0 }}
+							transition={{ ease: "easeInOut", duration: 0.3 }}
+						></motion.div>
+						<motion.div
+							animate={{ opacity: opened ? 0 : 1 }}
+							transition={{ ease: "easeInOut", duration: 0.3 }}
+						></motion.div>
+						<motion.div
+							className="origin-right"
+							animate={{ rotate: opened ? 45 : 0 }}
+							transition={{ ease: "easeInOut", duration: 0.3 }}
+						></motion.div>
 					</motion.button>
 				</div>
 
 				<motion.div
-					className="flex-col *:py-2 *:text-lg mt-4"
-					animate={{ height: opened ? "auto" : 0, overflow: "hidden" }}
+					className="flex-col *:py-2 *:text-lg overflow-hidden"
+					animate={{
+						height: opened ? "auto" : 0,
+						marginTop: opened ? 16 : 0,
+					}}
 					transition={{ ease: "easeInOut", duration: 0.3 }}
 				>
 					<div className="">Főoldal</div>
