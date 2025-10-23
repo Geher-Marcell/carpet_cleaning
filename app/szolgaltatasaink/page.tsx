a8ba81055569cdfd819f52069100523e9c12b5da
riseo <atti@atti.com>
2025-10-23 22:29:02 +0200

    design

--- app/szolgaltatasaink/page.tsx
+++ app/szolgaltatasaink/page.tsx
@@ -8,7 +8,7 @@ import PrimaryButton from "../component/buttons/PrimaryButton";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center pt-25">
-      <h1 className="text-4xl pb-7 font-medium">Szőnyegtisztítás</h1>
+      <h1 className="text-4xl pb-10 font-medium">Szőnyegtisztítás</h1>
 
       <div className="flex flex-wrap justify-center items-center gap-5">
         <InfoWidget
@@ -30,7 +30,7 @@ export default function Home() {
         />
       </div>
 
-      <h1 className="text-4xl pt-5 pb-5 font-medium">Autótisztítás</h1>
+      <h1 className="text-4xl pt-10 pb-10 font-medium">Autótisztítás</h1>
 
       <div className="flex flex-wrap justify-center items-center gap-5 pb-5">
         <InfoWidget
