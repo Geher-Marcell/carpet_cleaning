ca6a83e7ae8bb87e05a2d29a392df7f60b5cfe4e
riseo <atti@atti.com>
2025-10-23 20:05:24 +0200

    design change

--- app/szolgaltatasaink/page.tsx
+++ app/szolgaltatasaink/page.tsx
@@ -7,46 +7,50 @@ import { faHandsWash } from "@fortawesome/free-solid-svg-icons/faHandsWash";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5 justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl mt-5 mb-5">Szőnyegtisztítás</h1>
 
-      <InfoWidget
-        cim="Mélyszőnyeg Tisztítás"
-        leiras={"Alapos, mély tisztítás a szőnyegeinek."}
-        ikon={faBroom}
-      />
+      <div className="flex flex-wrap justify-center items-center gap-5">
+        <InfoWidget
+          cim="Mélyszőnyeg Tisztítás"
+          leiras={"Alapos, mély tisztítás a szőnyegeinek."}
+          ikon={faBroom}
+        />
 
-      <InfoWidget
-        cim="Folteltávolítás"
-        leiras={"Makacs foltok hatékony eltávolítása."}
-        ikon={faCircle}
-      />
+        <InfoWidget
+          cim="Folteltávolítás"
+          leiras={"Makacs foltok hatékony eltávolítása."}
+          ikon={faCircle}
+        />
 
-      <InfoWidget
-        cim="Kisállat Szag Eltávolítás"
-        leiras={"Távolítsa el a kisállat szagokat a szőnyegekből."}
-        ikon={faPaw}
-      />
+        <InfoWidget
+          cim="Kisállat Szag Eltávolítás"
+          leiras={"Távolítsa el a kisállat szagokat a szőnyegekből."}
+          ikon={faPaw}
+        />
+      </div>
 
       <h1 className="text-3xl mt-5 mb-5">Autótisztítás</h1>
 
-      <InfoWidget
-        cim="Belső Részletezés"
-        leiras={"Autója belső terének teljes tisztítása."}
-        ikon={faCar}
-      />
+      <div className="flex flex-wrap justify-center items-center gap-5 mb-5">
+        <InfoWidget
+          cim="Belső Részletezés"
+          leiras={"Autója belső terének teljes tisztítása."}
+          ikon={faCar}
+        />
 
-      <InfoWidget
-        cim="Külső Mosás & Viaszolás"
-        leiras={"Védő mosás és viasz a fényes felületért."}
-        ikon={faHandsWash}
-      />
+        <InfoWidget
+          cim="Külső Mosás & Viaszolás"
+          leiras={"Védő mosás és viasz a fényes felületért."}
+          ikon={faHandsWash}
+        />
 
-      <InfoWidget
-        cim="Teljes Körű Autómosás"
-        leiras={"Átfogó belső és külső tisztítás."}
-        ikon={faCar}
-      />
+        <InfoWidget
+          cim="Teljes Körű Autómosás"
+          leiras={"Átfogó belső és külső tisztítás."}
+          ikon={faCar}
+        />
+      </div>
 
       <Button
         szoveg="Kérjen ingyenes árajánlatot"
