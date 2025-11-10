296d2eb635556cf160167831ca3748853506ed2d
Geher-Marcell <geher.marcell@students.jedlik.eu>
2025-11-10 08:56:00 +0100

    - fixed order widget (popular was the issue) - added styling to order widget (to popular things)

--- app/component/pageModules/pricesModule.tsx
+++ app/component/pageModules/pricesModule.tsx
@@ -3,75 +3,75 @@ import { useEffect, useState } from "react";
 import OrderWidget from "../widgets/orderWidget";
 
 interface Service {
-	name: string;
-	price: string;
-	description: string;
-	unit: string;
-	hot?: boolean;
-	category: string;
+  name: string;
+  price: string;
+  description: string;
+  unit: string;
+  hot?: boolean;
+  category: string;
 }
 
 export default function PriceList() {
-	const [services, setServices] = useState<Record<string, Service[]>>({});
+  const [services, setServices] = useState<Record<string, Service[]>>({});
 
-	useEffect(() => {
-		fetch("/api/services") //GET request
-			.then((res) => res.json())
-			.then((data) => {
-				const grouped = data.services.reduce(
-					(acc: Record<string, Service[]>, service: Service) => {
-						const type =
-							service.category?.charAt(0).toUpperCase() +
-							service.category?.slice(1) +
-							"tisztítás";
-						if (!type) return acc; // Skip invalid categories
+  useEffect(() => {
+    fetch("/api/services") //GET request
+      .then((res) => res.json())
+      .then((data) => {
+        const grouped = data.services.reduce(
+          (acc: Record<string, Service[]>, service: Service) => {
+            const type =
+              service.category?.charAt(0).toUpperCase() +
+              service.category?.slice(1) +
+              "tisztítás";
+            if (!type) return acc; // Skip invalid categories
 
-						if (!acc[type]) {
-							acc[type] = [];
-						}
-						acc[type].push(service);
-						return acc;
-					},
-					{}
-				);
-				setServices(grouped);
-			});
-	}, []);
+            if (!acc[type]) {
+              acc[type] = [];
+            }
+            acc[type].push(service);
+            return acc;
+          },
+          {}
+        );
+        setServices(grouped);
+      });
+  }, []);
 
-	return (
-		<>
-			<div
-				className="p-10 flex flex-col gap-10 items-center justify-center
+  return (
+    <>
+      <div
+        className="p-10 flex flex-col gap-10 items-center justify-center
 					*:flex *:flex-col *:justify-center *:items-center *:gap-4"
-			>
-				{Object.keys(services).map((group, index) => (
-					<div key={index}>
-						<h3 className="text-2xl text-neutral-200 font-semibold pb-3">
-							{group || "Általános tisztítás"}
-						</h3>
-						<div className="flex flex-wrap gap-5 justify-center items-center">
-							{services[group]?.map((service, idx) => (
-								// <OrderWidget
-								// 	key={idx}
-								// 	title={service.name || "Unknown Service"}
-								// 	price={service.price || "-1"}
-								// 	unit={service.unit || "unit"}
-								// 	description={
-								// 		service.description || "No description available"
-								// 	}
-								// 	popular={service.hot}
-								// />
-								<div key={idx}>
-									{service.name}
-									<br />
-									{service.price}/{service.unit} <br />
-									{service.description}
-								</div>
-							))}
-						</div>
-					</div>
-				))}
-			</div>
-		</>
-	);
+      >
+        {Object.keys(services).map((group, index) => (
+          <div key={index}>
+            <h3 className="text-2xl text-neutral-200 font-semibold pb-3">
+              {group || "Általános tisztítás"}
+            </h3>
+            <div className="flex flex-wrap gap-5 justify-center items-center">
+              {services[group]?.map((service, idx) => (
+                <OrderWidget
+                  key={idx}
+                  title={service.name || "Unknown Service"}
+                  price={service.price || "-1"}
+                  unit={service.unit || "unit"}
+                  description={
+                    service.description || "No description available"
+                  }
+                  popular={service.hot || false}
+                />
+                // <div key={idx}>
+                //   {service.name}
+                //   <br />
+                //   {service.price}/{service.unit} <br />
+                //   {service.description}
+                // </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- app/component/widgets/orderWidget.tsx
+++ app/component/widgets/orderWidget.tsx
@@ -1,60 +1,62 @@
 import PrimaryButton from "@/app/component/buttons/PrimaryButton";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type widgetProps = {
-	title: string;
-	price: string;
-	description: string;
-	unit: string;
-	popular?: boolean;
+  title: string;
+  price: string;
+  description: string;
+  unit: string;
+  popular?: boolean;
 };
 
 const OrderWidget: React.FC<widgetProps> = ({
-	title,
-	price,
-	description,
-	unit,
-	popular,
+  title,
+  price,
+  description,
+  unit,
+  popular,
 }) => {
-	return (
-		<>
-			<div
-				className={`text-white  w-75 h-70 relative rounded-xl p-5 ${
-					popular
-						? "bg-[#381b08] bg-[linear-gradient(65deg,transparent_20%,rgba(253,199,0,0.1)_40%,transparent_60%)]"
-						: "bg-[#161b22] outline-1 outline-[#364050]"
-				}`}
-			>
-				{popular /*fdc700   */ && (
-					<div
-						className="absolute -inset-0.5 rounded-xl
+  return (
+    <>
+      <div
+        className={`text-white  w-75 h-70 relative rounded-xl p-5 ${
+          popular
+            ? "bg-[#381b08] bg-[linear-gradient(65deg,transparent_20%,rgba(253,199,0,0.1)_40%,transparent_60%)]"
+            : "bg-[#161b22] outline-1 outline-[#364050]"
+        }`}
+      >
+        {popular /*fdc700   */ && (
+          <div
+            className="absolute -inset-0.5 rounded-xl
             bg-[linear-gradient(65deg,transparent_20%,#fdc700_40%,transparent_60%)]
             -z-10 bg-[#db5d0b]"
-					></div>
-				)}
+          ></div>
+        )}
 
-				{popular && (
-					<p className="text-white text-xs font-bold bg-[#db5d0b] absolute -top-3 right-4 rounded-2xl w-fit p-1 pr-2 pl-2">
-						NÉPSZERŰ
-					</p>
-				)}
+        {popular && (
+          <p className="text-white text-xs font-bold bg-[#db5d0b] absolute -top-3 right-4 rounded-2xl w-fit p-1 pr-2 pl-2">
+            <FontAwesomeIcon icon={faFire} className="mr-1" />
+            NÉPSZERŰ
+          </p>
+        )}
 
-				<p className="text-xl font-medium mb-2">{title}</p>
+        <p className="text-xl font-medium mb-2">{title}</p>
 
-				<p className="text-4xl font-extrabold">
-					{price} Ft<span className="text-sm">/{unit}</span>
-				</p>
+        <p className="text-4xl font-extrabold">
+          {price} Ft<span className="text-sm font-medium">/{unit}</span>
+        </p>
 
-				<PrimaryButton label="Megrendelés" buttonClass="my-5" />
+        <PrimaryButton label="Megrendelés" buttonClass="my-5" />
 
-				<div className="flex items-center text-md font-medium my-1">
-					<FontAwesomeIcon icon={faCheck} className="text-[#db5d0b] mr-2.5" />
-					<p>{description}</p>
-				</div>
-			</div>
-		</>
-	);
+        <div className="flex items-center text-md font-medium my-1">
+          <FontAwesomeIcon icon={faCheck} className="text-[#db5d0b] mr-2.5" />
+          <p>{description}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrderWidget;
