0a1a664ab0b0592b0b70120d889d96b1be2264a5
Geher-Marcell <geher.marcell@students.jedlik.eu>
2025-11-11 09:13:03 +0100

    Refactor StepperNavbar to use forwardRef and useImperativeHandle for step management; update OrderPage to integrate step navigation and form handling logic.

--- app/component/utils/DynamicIcon.tsx
+++ app/component/utils/DynamicIcon.tsx
@@ -4,46 +4,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
 export default function DynamicFAIcon({
-  exportName,
-  size,
-  className,
+    exportName,
+    size,
+    className,
 }: {
-  exportName: string;
-  size?:
-    | "lg"
-    | "xs"
-    | "sm"
-    | "1x"
-    | "2x"
-    | "3x"
-    | "4x"
-    | "5x"
-    | "6x"
-    | "7x"
-    | "8x"
-    | "9x"
-    | "10x";
-  className?: string;
+    exportName: string;
+    size?:
+        | "lg"
+        | "xs"
+        | "sm"
+        | "1x"
+        | "2x"
+        | "3x"
+        | "4x"
+        | "5x"
+        | "6x"
+        | "7x"
+        | "8x"
+        | "9x"
+        | "10x";
+    className?: string;
 }) {
-  const [icon, setIcon] = useState<IconDefinition | null>(null);
-  console.log("Dynamically imported icon:", exportName);
+    const [icon, setIcon] = useState<IconDefinition | null>(null);
+    // console.log("Dynamically imported icon:", exportName);
 
-  useEffect(() => {
-    let mounted = true;
-    import("@fortawesome/free-solid-svg-icons").then((mod) => {
-      //   console.log("Available keys in module:", Object.keys(mod));
-      const modTyped = mod as unknown as Record<
-        string,
-        IconDefinition | undefined
-      >;
-      const found = modTyped[exportName];
-      if (mounted && found) setIcon(found);
-    });
-    return () => {
-      mounted = false;
-    };
-  }, [exportName]);
+    useEffect(() => {
+        let mounted = true;
+        import("@fortawesome/free-solid-svg-icons").then((mod) => {
+            //   console.log("Available keys in module:", Object.keys(mod));
+            const modTyped = mod as unknown as Record<
+                string,
+                IconDefinition | undefined
+            >;
+            const found = modTyped[exportName];
+            if (mounted && found) setIcon(found);
+        });
+        return () => {
+            mounted = false;
+        };
+    }, [exportName]);
 
-  if (!icon) return null;
-  return <FontAwesomeIcon icon={icon} size={size} className={className} />;
+    if (!icon) return null;
+    return <FontAwesomeIcon icon={icon} size={size} className={className} />;
 }
